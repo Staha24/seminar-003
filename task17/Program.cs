2f2362e855763b26ab270c84c27d39396e650115
stanislav <79920025475@yandex.ru>
2022-11-30 21:29:38 +0500

    task17

---
+++ task17/Program.cs
@@ -0,0 +1,14 @@
+﻿// task17
+Console.Write("Введите координату Х: ");
+double x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату Y: ");
+double y = Convert.ToDouble(Console.ReadLine());
+
+if (x > 0 && y > 0)
+    Console.Write( "1");
+else if ( x < 0 && y > 0)
+    Console.WriteLine("2");
+else if (  x < 0 && y < 0)
+    Console.WriteLine("3");
+else 
+    Console.WriteLine("4");   
