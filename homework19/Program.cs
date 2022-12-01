b11bac778134d97593278db9bf8814f220602b86
stanislav <79920025475@yandex.ru>
2022-12-01 19:34:15 +0500

    homework19

---
+++ homework19/Program.cs
@@ -0,0 +1,8 @@
+﻿// task19
+Console.Write (" Введите пятизначное число: ");
+int n = Convert.ToInt32 (Console.ReadLine ());
+
+if (n / 10000 == n % 10 && ( n / 1000) % 10 == (n /10 % 10))
+    Console.WriteLine ("да");
+else
+    Console.WriteLine ("нет");
